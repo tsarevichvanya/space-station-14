ca0ce6829e919da374a3e87f5e91fba22db092d2
Mrinank Kiran <mrinankkiran13@gmail.com>
2025-06-17 01:52:57 -0400

    modified:   Content.Server/Mind/MindSystem.cs

--- Content.Server/Mind/MindSystem.cs
+++ Content.Server/Mind/MindSystem.cs
@@ -259,8 +259,8 @@ public sealed class MindSystem : SharedMindSystem
             component!.Mind = mindId;
             mind.OwnedEntity = entity;
             mind.OriginalOwnedEntity ??= GetNetEntity(mind.OwnedEntity);
-            Entity<MindComponent> mindEnt = (mindId, mind);
-            Entity<MindContainerComponent> containerEnt = (entity.Value, component);
+            //Entity<MindComponent> mindEnt = (mindId, mind);
+            //Entity<MindContainerComponent> containerEnt = (entity.Value, component);
             RaiseLocalEvent(entity.Value, new MindAddedMessage(mindEnt, containerEnt));
             RaiseLocalEvent(mindId, new MindGotAddedEvent(mindEnt, containerEnt));
             Dirty(entity.Value, component);
